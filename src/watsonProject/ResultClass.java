ce06d1ce1d36b1635f7de94356b63c567fffccd6
awoka <akatek1@email.arizona.edu>
2020-12-05 16:37:46 -0700

    Finalized Index Creation Part

---
+++ src/watsonProject/ResultClass.java
@@ -0,0 +1,7 @@
+package watsonProject;
+import org.apache.lucene.document.Document;
+
+public class ResultClass {
+    Document DocName;
+    double docScore = 0;
+}
--- src/watsonProject/watson.java
+++ src/watsonProject/watson.java
@@ -1,8 +1,11 @@
 package watsonProject;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.List;
 
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.index.IndexReader;
 import org.apache.lucene.store.Directory;
 
 public class watson {
@@ -14,7 +17,16 @@ public class watson {
 	Directory index;
 	
 	public static void main(String[] args) {
-		index Ind = new index("src/docs");
+		index ind = new index("src/docs");
+		/*List<ResultClass> test = null;
+		try {
+			test = ind.runQuery("the");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		for (ResultClass result : test) {
+			System.out.println(result.DocName.get("docName") + ": " + result.docScore);
+		}*/
 	}
 	
 	private void buildIndex() {
